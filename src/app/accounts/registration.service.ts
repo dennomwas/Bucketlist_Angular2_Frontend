c0dd7b403ee89241007dc65d610fc354a082ca67
dennomwas <githinji.mwangi@gmail.com>
2017-05-15 12:32:41 +0300

    [Feature] Add Registration service

--- src/app/accounts/registration.service.ts
+++ src/app/accounts/registration.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+import { Http, Response, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RegistrationService {
+  private headers: Headers;
+  private registerUrl = 'http://127.0.0.1:5000/auth/register/';
 
-  constructor() { }
+  constructor(private _http: Http, private _router: Router) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Access-Control-Allow-Origin', '*');
+  }
+  registerUser(fullname: string, username: string, password: string) {
+    return this._http.post(this.registerUrl,
+          JSON.stringify({fullname, username, password}),
+                         {headers: this.headers})
+      .map((response) => {
+          console.log(response);
+          this._router.navigate(['login']);
+            return response.json().username;
+    })
+      // .do(res => console.log('All: ' + JSON.stringify(response)))
+      .catch(this.handleError);
+  }
+  private handleError(error: Response) {
+    console.log(error);
+    return Observable.throw(error.json().error || 'Server Error');
+  }
 
 }
