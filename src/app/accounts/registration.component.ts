caaac0210ec627804fda23e057f410e266235845
dennomwas <githinji.mwangi@gmail.com>
2017-05-15 12:52:33 +0300

    [Feature] Add Registration component

--- src/app/accounts/registration.component.ts
+++ src/app/accounts/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RegistrationService} from './registration.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  title = 'Enter your details to register!'
+  fullname: string;
+  username: string;
+  password: string;
 
-  constructor() { }
+
+  constructor(private _registrationService: RegistrationService,
+              private _router: Router) { }
+  registration(): void {
+    this._registrationService.registerUser(this.fullname, this.username, this.password)
+      .subscribe(
+        (data) => {
+          if (data) {
+            console.log(data);
+            this._router.navigate(['login']);
+          }
+        },
+      (error) => {
+        if (error) {
+          this.errorHandler(error);
+          console.log(error);
+        }
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  errorHandler(error: any): void {
+    console.log(error);
   }
 
 }
