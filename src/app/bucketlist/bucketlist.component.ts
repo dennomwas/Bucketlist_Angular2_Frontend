8f57d0bff761656b6af1d3f1cc971d7d4fc44312
dennomwas <githinji.mwangi@gmail.com>
2017-05-16 16:27:42 +0300

    [Feature] Add, Delete, and Update features on bucket lists

--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { BucketlistService } from './bucketlist.service';
 import { IBucketlist } from './bucketlist';
+import { LoginService } from '../accounts/login.service';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-bucketlist',
@@ -13,14 +16,58 @@ export class BucketlistComponent implements OnInit {
   title = 'Bucketlists';
   errorMessage: string;
   bucketlists: IBucketlist[];
+  nameToAdd: string;
+  nameToUpdate: string;
+  // bucket_id: number;
 
-  constructor(private _bucketlistservice: BucketlistService) { }
 
-  ngOnInit(): void {
+
+  constructor(private _bucketlistservice: BucketlistService, private _loginService: LoginService, private _router: Router) { }
+
+  ngOnInit() {
+    this.getBucketLists();
+  }
+  addBucketlist() {
+    this._bucketlistservice.addBucket(this.nameToAdd)
+      .subscribe(
+        addBucket => {
+          this.bucketlists = addBucket;
+          this.getBucketLists();
+        }
+      );
+  }
+  getBucketLists() {
     this._bucketlistservice.getAllBuckets()
       .subscribe(
-        bucketlists => this.bucketlists,
+        bucketlists => {
+          this.bucketlists = bucketlists;
+          // console.log(this.bucketlists);
+        },
         error => this.errorMessage = <any>error);
   }
+  deleteBucketList(id: number) {
+    console.log(id);
+    this._bucketlistservice.deleteBucket(id)
+      .subscribe(
+        deleted => {
+          if (deleted) {
+            this.getBucketLists();
+          }
+    });
+  }
+  updateBucketlist(id: number) {
+    this._bucketlistservice.updateBucket(id, this.nameToUpdate)
+      .subscribe(
+        updated => {
+          if (updated) {
+            this.getBucketLists();
+          }
+        }
+      );
+  }
 
+  logoutUser() {
+    this._loginService.logoutUser();
+    this._router.navigate(['login']);
+  }
 }
--- src/app/bucketlist/bucketlist.service.ts
+++ src/app/bucketlist/bucketlist.service.ts
@@ -14,17 +14,39 @@ export class BucketlistService {
   private headers: Headers;
   private bucketlistUrl = 'http://127.0.0.1:5000/bucketlists/';
 
+  tokenAuth = localStorage.getItem('token');
+
   constructor(private _http: Http) {
     this.headers = new Headers();
     this.headers.append('Accept', 'application/json');
     this.headers.append('Content-Type', 'application/json');
     this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Authorization', 'Bearer ' + this.tokenAuth);
   }
-
   getAllBuckets(): Observable<IBucketlist[]> {
-    return this._http.get(this.bucketlistUrl, { headers: this.headers })
-      .map((response: Response) => <IBucketlist[]>response.json())
-      .do(data => console.log('All: ' + JSON.stringify(data)))
+    return this._http.get(this.bucketlistUrl,
+           { headers: this.headers })
+      .map((response: Response) => <IBucketlist[]>response.json().Results)
+      // .do(data => console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+  deleteBucket(bucket_id: number) {
+    const deleteUrl = this.bucketlistUrl + `${bucket_id}`;
+
+    return this._http.delete(deleteUrl, {headers: this.headers})
+           .catch(this.handleError);
+  }
+  addBucket(bucket_name: string) {
+    const json_data = JSON.stringify({
+      'bucket_name': bucket_name
+    });
+    return this._http.post(this.bucketlistUrl, json_data, {headers: this.headers})
+      .catch(this.handleError);
+  }
+  updateBucket(bucket_id: number, bucket_name: string) {
+    const updateUrl = this.bucketlistUrl + bucket_id;
+    const newUpdate = JSON.stringify({'bucket_name': bucket_name});
+    return this._http.put(updateUrl, newUpdate , {headers: this.headers})
       .catch(this.handleError);
   }
   private handleError(error: Response) {
