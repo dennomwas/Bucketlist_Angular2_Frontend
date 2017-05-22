7c3d77710b43abe7ae51f17dadb446849d1f80a5
dennomwas <githinji.mwangi@gmail.com>
2017-05-23 01:03:47 +0300

    [Feature] Refactor bucketlist routes

--- src/app/app.route.module.ts
+++ src/app/app.route.module.ts
@@ -2,15 +2,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './accounts/login.component';
-import { BucketlistComponent } from './bucketlist/bucketlist.component';
 import { RegistrationComponent } from './accounts/registration.component';
+import { BucketlistComponent } from './bucketlist/bucketlist.component';
+import { BucketitemsComponent } from './bucketitems/bucketitems.component';
+
+import {AuthGuard} from './authguard/auth.guard';
 
 
 export const routes: Routes = [
                                 {path: '', redirectTo: 'login', pathMatch: 'full'},
                                 {path: 'login', component: LoginComponent},
                                 {path: 'register', component: RegistrationComponent},
-                                {path: 'bucketlists', component: BucketlistComponent}
+                                {path: 'bucketlists', component: BucketlistComponent, canActivate: [AuthGuard]},
+                                {path: 'bucketlists/:id', component: BucketlistComponent, canActivate: [AuthGuard]},
+                                {path: 'bucketlists/:id/items', component: BucketitemsComponent, canActivate: [AuthGuard]}
                               ];
 
 @NgModule({
@@ -21,8 +26,10 @@ export const routes: Routes = [
 
 export class AppRouteModule {}
 
-export const routingComponents = [BucketlistComponent,
+export const routingComponents = [
                                   LoginComponent,
-                                  RegistrationComponent
+                                  RegistrationComponent,
+                                  BucketlistComponent,
+                                  BucketitemsComponent
                                  ];
 
